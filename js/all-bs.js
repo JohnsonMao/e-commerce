d3e40855ee3cb1ce833c42389996deed72a0f43b
JohnsonMao <86179381+JohnsonMao@users.noreply.github.com>
2021-07-05 19:01:24 +0800

    update animation

--- js/all-bs.js
+++ js/all-bs.js
@@ -19,4 +19,25 @@ $(document).ready(function(){
         $(this).removeClass('show');
     });
 
+    // product inc & dec
+    $('.inc').click(function(){
+        let n = $(this).parents('ul').siblings('.product-num').val();
+        n++;
+        $(this).parents('ul').siblings('.product-num').val(n);
+    });
+
+    $('.dec').click(function(){
+        let n = $(this).parents('ul').siblings('.product-num').val();
+        // 0 以下不倒扣
+        if(n==0){
+            return;
+        }
+        n--;
+        $(this).parents('ul').siblings('.product-num').val(n);
+    });
+
+    // 加入購物車後表單歸 0 
+    $('.add-shopping').click(function(){
+        $(this).siblings('.product-num').val(0);
+    })
 });
