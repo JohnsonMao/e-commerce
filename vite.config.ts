e632e75f9cdee8a182a7bd300ad7aae72ea54039
Johnson Mao <tutelary.maomao@gmail.com>
2023-01-20 20:24:33 +0800

    init: eslint & base scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+	const isDev = mode === 'development';
+	return {
+		base: isDev ? '/' : '/scrum_f2e/',
+		plugins: [react()],
+		server: {
+			host: '0.0.0.0',
+			open: true
+		}
+	};
+});
